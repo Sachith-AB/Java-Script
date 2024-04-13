2d057fd4400d25472261e65eaecf4b525a20cbbd
sachith avintha <sachithavintha35@gmail.com>
2024-04-14 00:21:46 +0530

    first code js

---
+++ arraay.js
@@ -0,0 +1,8 @@
+var array=[10,20,30];
+console.log(array);
+array[0]=11;
+console.log(array);
+
+array2=[[1,2,3],[4,5,6],[7,8,0]];
+console.log(array2);
+array2[2][2]=10;
---
+++ text2.js
@@ -0,0 +1,6 @@
+function world(noun,adjective,verb,adverb){
+    var result;
+    result="The "+adjective +" "+ noun +" "+ verb +" " + adverb+"."
+    return result;
+}
+console.log(world("dog","white","run","fast"));
